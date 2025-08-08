ede54bc3f30d2baf9c69e1c523f3ef5be2c67373
mihiran <mihiransamarasinghe5@gmail.com>
2025-08-08 21:47:35 +0530

    remove icons

--- front-end/src/pages/Manager.jsx
+++ front-end/src/pages/Manager.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import SideBar from '../components/SideBar'
+import { FaTachometerAlt, FaUser, FaCog } from 'react-icons/fa';
+
+const ManagerLinks = [
+  { label: 'link1', href: '/admin/dashboard', icon: <FaTachometerAlt /> },
+  { label: 'link2', href: '/admin/users', icon: <FaUser /> },
+  { label: 'link3', href: '/admin/settings', icon: <FaCog /> },
+];
 
 function Manager() {
   return (
-    <div>Manager</div>
+    <div>
+      <SideBar title="Manager Panel" links={ManagerLinks} />
+    </div>
   )
 }
 
