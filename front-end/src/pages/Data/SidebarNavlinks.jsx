4af2d1a96d7b78f77001fcc35cf7e83621678ba4
mihiran <hansamihiran2002@gmail.com>
2025-08-11 00:38:08 +0530

    update creating new navlinks

--- front-end/src/pages/Data/SidebarNavlinks.jsx
+++ front-end/src/pages/Data/SidebarNavlinks.jsx
@@ -1,8 +1,8 @@
 import { FaTachometerAlt, FaUser, FaCog } from 'react-icons/fa';
 
 export const ManagerLinks = [
-  { label: 'Dashboard', href: '/admin/dashboard', icon: <FaTachometerAlt /> },
-  { label: 'Users', href: '/admin/users', icon: <FaUser /> },
+  { label: 'Dashboard', href: '/manager', icon: <FaTachometerAlt /> },
+  { label: 'Employee Management', href: '/manager/em', icon: <FaUser /> },
   { label: 'Settings', href: '/admin/settings', icon: <FaCog /> },
 ];
 
