4ffdace64dfd77d8e72064128203c4b45ca68db7
mihiran <hansamihiran2002@gmail.com>
2025-08-10 23:28:10 +0530

    update

---
+++ front-end/src/pages/Manager/Manager.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SideBar from '../../components/SideBar'
+
+import UserProfile from '../../components/UserProfile';
+import {ManagerLinks} from '../../pages/Data/SidebarNavlinks';
+
+
+function Manager() {
+  return (
+    <div>
+      <SideBar title="Manager Panel" links={ManagerLinks} />
+      <div className='p-18'>
+        {/* <UserProfile/> */}
+      </div>
+      
+    </div>
+  )
+}
+
+export default Manager
