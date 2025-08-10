9936372f10b4adec1aafe51f3f07e345ba8d6139
mihiran <hansamihiran2002@gmail.com>
2025-08-10 23:28:03 +0530

    create centalize page for arrays

---
+++ front-end/src/pages/Data/SidebarNavlinks.jsx
@@ -0,0 +1,7 @@
+import { FaTachometerAlt, FaUser, FaCog } from 'react-icons/fa';
+
+export const ManagerLinks = [
+  { label: 'Dashboard', href: '/admin/dashboard', icon: <FaTachometerAlt /> },
+  { label: 'Users', href: '/admin/users', icon: <FaUser /> },
+  { label: 'Settings', href: '/admin/settings', icon: <FaCog /> },
+];
