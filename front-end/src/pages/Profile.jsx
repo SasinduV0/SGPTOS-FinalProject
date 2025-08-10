09b28d7b813c08f7b9bfc9e5959c70fb6d6f679e
mihiran <hansamihiran2002@gmail.com>
2025-08-11 00:22:14 +0530

    enhance styles

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import UserProfile from '../components/UserProfile'
+import SideBar from '../components/SideBar'
+import {ProfileLinks} from '../pages/Data/SidebarNavlinks'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+        <SideBar title="Settings" links={ProfileLinks}/>
+        <UserProfile/>
+    </div>
   )
 }
 
