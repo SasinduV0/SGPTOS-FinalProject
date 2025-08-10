b8c3c869a8329a0b6bea3f34c35adf62f3ff6c12
mihiran <hansamihiran2002@gmail.com>
2025-08-10 23:19:49 +0530

    create get and put route

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -6,21 +6,26 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
+        trim: true
     },
     userID:{
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password:{
         type: String,
-        required: true
+        required: true,
+        minlength: 6
     },
     role: {
         type: String,
         required: true,
-        enum: ["qc","supervisor", "manager", "admin","live-dashboard"],
-        default: "user"
+        enum: ["qc", "supervisor", "manager", "admin", "live-dashboard"],
+        default: "qc"
     }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);
