9404da60c54527c9e95efb55e50159fa787af47c
DulaKavindi <dularikavindi6@gmail.com>
2025-08-14 23:12:48 +0530

    update employeeDetails file

---
+++ front-end/src/pages/Admin/EmployeeDetails.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function EmployeeDetails() {
+
+  return (
+    <div className="mb-4">
+
+        <label className="block mb-2 front-medium">
+            Employee Name
+        </label>
+
+        <input
+            type="text"
+            value={employeeName}
+            onChange={()=> setEmployeeName (e.target.value)}
+            className="border p-2 rounded w-64"
+            placeholder="Enter employee name"/>
+
+    </div>
+  )
+}
+
+export default EmployeeDetails
