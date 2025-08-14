3b3656f8cdadeaecc5654f794abb8940b9b5178f
DulaKavindi <dularikavindi6@gmail.com>
2025-08-15 01:28:54 +0530

    Update AdminNav file

--- front-end/src/components/AdminNav.jsx
+++ front-end/src/components/AdminNav.jsx
@@ -10,12 +10,15 @@ function AdminNav({currentStep}) {
   return (
     <div className="flex gap-4 mb-6">
 
-      {icon.map((lable, index) => (
+      {icon.map((label, index) => (
 
         <div
           key={index}
           className={`px-4 py-2 rounded ${
-            step === index + 1 ? "bg-blue-500 text-white":"bg-gray-500"}`}>
+            currentStep === index + 1 ? "bg-blue-500 text-white":"bg-gray-500"}`}
+              >
+
+                {label}
 
           </div>
 
