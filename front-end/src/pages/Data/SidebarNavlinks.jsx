d3cb5afc806bd3755df6d59fd6da5fc6f7356bcb
Mihiran1 <hansamihiran2002@gmail.com>
2025-08-15 21:13:54 +0530

    add dashboard icon

--- front-end/src/pages/Data/SidebarNavlinks.jsx
+++ front-end/src/pages/Data/SidebarNavlinks.jsx
@@ -1,15 +1,18 @@
 import { FaTachometerAlt, FaUser, FaCog } from 'react-icons/fa';
+import { MdDashboard } from "react-icons/md";
 
 export const ManagerLinks = [
   { label: 'Dashboard', href: '/manager', icon: <FaTachometerAlt /> },
   { label: 'Employee Management', href: '/manager/em', icon: <FaUser /> },
   { label: 'Employee Efficiency', href: '/manager/ee', icon: <FaCog /> },
+  { label: 'Live Dashboard', href: '/live-dashboard', icon: <MdDashboard /> },
 ];
 
 export const adminLinks = [
   { label: 'Home', href: '/admin/dashboard', icon: <FaTachometerAlt /> },
   { label: 'Employee', href: '/admin/users', icon: <FaUser /> },
   { label: 'Settings', href: '/admin/settings', icon: <FaCog /> },
+  { label: 'Live Dashboard', href: '/live-dashboard', icon: <MdDashboard /> },
 ];
 
 export const ProfileLinks = [
