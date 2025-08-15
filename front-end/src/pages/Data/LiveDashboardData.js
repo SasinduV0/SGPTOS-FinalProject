d656b44552e7b081eec6760aaee222ab67143de5
Mihiran1 <hansamihiran2002@gmail.com>
2025-08-15 20:38:09 +0530

    create Line card data as seperate js file

---
+++ front-end/src/pages/Data/LiveDashboardData.js
@@ -0,0 +1,16 @@
+// data.js
+export const leadingLineData = [
+  {
+    lineName: "Line 4",
+    target: "Target achieved",
+    employees: [
+      { name: "Employee 1", pcs: 81 },
+      { name: "Employee 2", pcs: 81 },
+      { name: "Employee 3", pcs: 81 },
+      { name: "Employee 4", pcs: 81 },
+      { name: "Employee 5", pcs: 81 },
+      { name: "Employee 6", pcs: 81 },
+      { name: "Employee 7", pcs: 81 },
+    ],
+  }
+];
