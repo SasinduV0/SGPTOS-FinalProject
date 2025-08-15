681f3f978e9e7eb26f17f4b1bd604aca40423b89
Mihiran1 <hansamihiran2002@gmail.com>
2025-08-15 20:37:46 +0530

    update live dashboard for Line card

--- front-end/src/pages/LiveDashboard.jsx
+++ front-end/src/pages/LiveDashboard.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 // import {Chart as ChartJS} from 'chart.js/auto';
 // import {Bar, Doughut, Line} from 'react-chartjs-2'
 import ProductionDashboard from '../components/live-dashboard/ProductionDashboard'
+import LeadingLineCard from '../components/live-dashboard/LeadingLineCard'
 
 function LiveDashboard() {
   return (
     <div>
-      <div className='h-screen w-full flex items-center justify-center gap-4 bg-gray-300'>
+      <div className='p-8 w-full flex items-center justify-center gap-4 bg-gray-300'>
         {/* Left-section */}
         <div className='w-[30%] flex flex-col gap-4'>
-          <div className='bg-blue-800 rounded-xl shadow-2xl p-4 h-[250px]'>
-            <h2 className='text-white text-lg font-semibold flex items-center justify-center'>Leading Line</h2>
-            <p className="text-sm">Line 4 : Target achieved</p>
+          <div className='bg-blue-800 rounded-xl shadow-2xl p-4 h-[350px]'>
+            <LeadingLineCard/>
           </div>
           <div className='bg-white rounded-xl shadow-2xl p-4 h-[200px]'>chart 3</div>
           <div className='bg-white rounded-xl shadow-2xl p-4 h-[200px]'>chart 3</div>
@@ -20,7 +20,7 @@ function LiveDashboard() {
         {/* Middle-section */}
         <div className='w-[30%]'>
           <div className='bg-white rounded-xl shadow-2xl p-4 h-[680px]'>chart 2
-            <ProductionDashboard/>
+            {/* <ProductionDashboard/> */}
           </div>
         </div>
 
