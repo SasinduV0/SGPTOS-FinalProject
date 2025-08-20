87ea1826994b92bcb58bdab74a34ad33653b45a2
Mihiran1 <hansamihiran2002@gmail.com>
2025-08-20 22:46:47 +0530

    modified iot routes

--- back-end/models/iot.js
+++ back-end/models/iot.js
@@ -1,18 +1,44 @@
 const mongoose = require("mongoose");
 
 const defectSchema = new mongoose.Schema({
-  sewingDefect: { type: Boolean, default: false },
-  coloringDefect: { type: Boolean, default: false },
-  cuttingDefect: { type: Boolean, default: false },
+  sewingDefect: { 
+    type: Boolean, 
+    default: false },
+
+  coloringDefect: { 
+    type: Boolean, 
+    default: false },
+
+  cuttingDefect: { 
+    type: Boolean, 
+    default: false },
+
   default: { type: String, default: "none" },
 });
 
 const iotDataSchema = new mongoose.Schema({
-  userRFID: { type: String, required: true },
-  garmentRFID: { type: String, required: true },
-  stationRFID: { type: String, required: true },
-  time: { type: Date, default: Date.now },
-  defects: { type: defectSchema, required: true },
+
+  ID: { 
+    type: String, 
+    required: true },
+
+  tagUID:{
+    type: String,
+    required:true
+  },
+
+  stationID: { 
+    type: String, 
+    required: true },
+    
+  timeStamp: { 
+    type: Date, 
+    default: Date.now },
+
 });
+
+const stationSchema =  new mongoose.Schema({
+  
+})
 
 module.exports = mongoose.model("IOTData", iotDataSchema);
