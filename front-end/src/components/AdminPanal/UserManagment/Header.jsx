24aaa88e2a13e1b10a45925c2b669c07a8b5e58e
DulaKavindi <dularikavindi6@gmail.com>
2025-08-27 11:22:57 +0530

    fixed errors

--- front-end/src/components/AdminPanal/UserManagment/Header.jsx
+++ front-end/src/components/AdminPanal/UserManagment/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Plus } from 'lucide-react';
 
-const Header = ({ title, onAddClick, addButtonText = "Add Entry", icon }) => {
+const Header = ({ icon }) => {
   return (
     <div className="flex items-center justify-between p-6 border-b">
       <div className="flex items-center gap-3">
