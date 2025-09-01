a652b365aa7dd45593aefa3d9178e2500812547e
Mihiran1 <hansamihiran2002@gmail.com>
2025-09-01 19:02:11 +0530

    modified bar chart

--- front-end/src/pages/LiveDashboard.jsx
+++ front-end/src/pages/LiveDashboard.jsx
@@ -10,43 +10,55 @@ import Greetings from '../components/live-dashboard/Greeting'
 import TopEmployees from '../components/live-dashboard/TopEmployees'
 import LineTargetChart from '../components/live-dashboard/LineTargetChart'
 
-function LiveDashboard() {
+function LiveDashboard({ lineData }) {
+
+  const lineTargets = {
+  1: 1000,
+  2: 800,
+  3: 900,
+  4: 1100,
+  5: 950,
+  6: 1050,
+  7: 700,
+  8: 850,
+};
+
   return (
     <div>
-      <div className='p-5 w-full flex items-center justify-center gap-4 bg-gray-300'>
+      <div className='p-5 w-full flex items-center justify-center gap-6 bg-gray-100 '>
         {/* Left-section */}
         <div className='w-[30%] flex flex-col gap-4'>
-          <div className='bg-blue-800 rounded-xl shadow-2xl p-4 h-[350px]'>
+          <div className='bg-blue-800 rounded-xl shadow-lg p-4 h-[310px] shadow-gray-500'>
             <LeadingLineCard/>
           </div>
-          <div className='bg-green-600 rounded-xl shadow-2xl p-4 h-[280px]'>
+          <div className='bg-yellow-300 rounded-xl shadow-lg p-4 h-[250px] shadow-gray-500'>
             {/* <FastestEmployees/> */}
             <TopEmployees/>
           </div>
-          <div className='bg-white rounded-xl shadow-2xl p-4 h-[270px]'>chart 3</div>
+          <div className='bg-white rounded-xl shadow-lg p-4 h-[240px] shadow-gray-500'>chart 3</div>
         </div>
 
         {/* Middle-section */}
-        <div className="w-[30%]">
-          <div className="bg-gray-400 rounded-xl shadow-2xl p-4 h-[930px] flex items-center justify-center gap-25 flex-col">
-            <Greetings/>
-            <OverallTargetChart />
+        <div className="w-[35%]">
+          <div className="bg-white rounded-xl shadow-lg  h-[830px] flex items-center justify-center flex-col shadow-gray-500">
+              <Greetings/>
+               <OverallTargetChart lineTargets={lineTargets} />
              <div className="">
       <     LineTargetChart />
-    </div>
-          </div>
-        </div>
+      </div>
+      </div>
+      </div>
 
 
         {/* Right-section */}
         <div className='w-[30%] flex flex-col gap-4'>
-          <div className=' bg-red-700 rounded-xl shadow-2xl p-4 h-[350px]'>
+          <div className=' bg-red-700 rounded-xl shadow-lg p-4 h-[310px] shadow-gray-500'>
             <FollowingLine/>
           </div>
-          <div className='bg-yellow-300 rounded-xl shadow-2xl p-4 h-[280px]'>
+          <div className='bg-yellow-300 rounded-xl shadow-lg p-4 h-[250px] shadow-gray-500'>
             chart 3
           </div>
-          <div className='bg-white rounded-xl shadow-2xl p-4 h-[270px]'>chart 3</div>
+          <div className='bg-white rounded-xl shadow-lg p-4 h-[240px] shadow-gray-500'>chart 3</div>
         </div>
       </div>
     </div>
